d52648f5345c9b6d36987f2030ae57fb0f9dc698
Jishan Ahmed <workwithjishan.info@gmail.com>
2024-01-22 20:49:38 +0530

    completed skills section

--- components/skills.tsx
+++ components/skills.tsx
@@ -33,7 +33,7 @@ export default function Skills() {
       <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
         {skillsData.map((skill, index) => (
           <motion.li
-            className=" borderBlack rounded-xl px-5 py-3"
+            className="bg-white borderBlack rounded-xl px-5 py-3"
             key={index}
             variants={fadeInAnimationVariants}
             initial="initial"
