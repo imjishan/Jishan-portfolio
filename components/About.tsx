350d6a96ef431230e6fd38d27276a47c04479ff4
Jishan Ahmed <workwithjishan.info@gmail.com>
2024-01-26 03:33:32 +0530

    configure eslint

--- components/About.tsx
+++ components/About.tsx
@@ -1,36 +1,49 @@
 "use client";
+
+import React from "react";
+import SectionHeading from "./SectionHeading";
+import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
 
-const About = () => {
+export default function About() {
   const { ref } = useSectionInView("About");
+
   return (
-    <section
+    <motion.section
       ref={ref}
-      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-8 font-light scroll-mt-28"
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
       id="about"
     >
-      <h2 className="text-3xl font-medium capitalize mb-8 text-center">
-        About me
-      </h2>
-      <p className="mb-4 text-gray-800 dark:text-gray-200">
-        I'm Jishan, currently navigating the academic landscape while delving
-        deep into the world of web development. My days are filled with coding
-        adventures, where I harness the power of{" "}
-        <span className="font-semibold">
-          NextJS, React, NodeJS, ExpressJS and MongoDB
-        </span>{" "}
-        to bring ideas to life. Crafting seamless user experiencing and solving
-        complex challenges fuel my passion for web development.
+      <SectionHeading>About me</SectionHeading>
+      <p className="mb-3">
+        After graduating with a degree in{" "}
+        <span className="font-medium">Accounting</span>, I decided to pursue my
+        passion for programming. I enrolled in a coding bootcamp and learned{" "}
+        <span className="font-medium">full-stack web development</span>.{" "}
+        <span className="italic">My favorite part of programming</span> is the
+        problem-solving aspect. I <span className="underline">love</span> the
+        feeling of finally figuring out a solution to a problem. My core stack
+        is{" "}
+        <span className="font-medium">
+          React, Next.js, Node.js, and MongoDB
+        </span>
+        . I am also familiar with TypeScript and Prisma. I am always looking to
+        learn new technologies. I am currently looking for a{" "}
+        <span className="font-medium">full-time position</span> as a software
+        developer.
       </p>
-      <p className="text-gray-800 dark:text-gray-200">
-        Beyond the screen, I find my escape in the virtual realms of video
-        games, immersing myself in captivating storylines. To maintain balance,
-        I'm equally committed to physical well-being, incorporating workouts
-        into my routine. Join me in this journey where technology meets play,
-        and let's explore the exciting possibilities in the ever-evolving web
-        development landscape.
+
+      <p>
+        <span className="italic">When I'm not coding</span>, I enjoy playing
+        video games, watching movies, and playing with my dog. I also enjoy{" "}
+        <span className="font-medium">learning new things</span>. I am currently
+        learning about{" "}
+        <span className="font-medium">history and philosophy</span>. I'm also
+        learning how to play the guitar.
       </p>
-    </section>
+    </motion.section>
   );
-};
-export default About;
+}
