f5bf670a523119cfd11ac34d33df5e823eddd282
Jishan Ahmed <workwithjishan.info@gmail.com>
2024-01-26 03:40:13 +0530

    escaped ' with " &apos; "

--- components/About.tsx
+++ components/About.tsx
@@ -37,12 +37,12 @@ export default function About() {
       </p>
 
       <p>
-        <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
-        <span className="font-medium">learning new things</span>. I am currently
-        learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
+        <span className="italic">When I&apos;m not coding</span>, I enjoy
+        playing video games, watching movies, and playing with my dog. I also
+        enjoy <span className="font-medium">learning new things</span>. I am
+        currently learning about{" "}
+        <span className="font-medium">history and philosophy</span>. I&apos;m
+        also learning how to play the guitar.
       </p>
     </motion.section>
   );
--- components/intro.tsx
+++ components/intro.tsx
@@ -59,7 +59,7 @@ const Intro = () => {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Jishan.</span> I'm a{" "}
+        <span className="font-bold">Hello, I&apos;m Jishan.</span> I&apos;m a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
         <span className="font-bold">1 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
